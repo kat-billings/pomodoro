cf845931fdce12deaddce41b0c6f9250ee1ea856
kat-billings <katbillings235@gmail.com>
2022-01-10 16:33:33 -0600

    updated HTML

--- src/main/java/com/katbillings/pomodoro/PomodoroApplication.java
+++ src/main/java/com/katbillings/pomodoro/PomodoroApplication.java
@@ -2,12 +2,17 @@ package com.katbillings.pomodoro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Timer;
 
 @SpringBootApplication
 public class PomodoroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PomodoroApplication.class, args);
+	}
+
+	public static int timer() {
+		return 0;
 	}
 
 }
--- src/main/java/com/katbillings/pomodoro/controllers/Home.java
+++ src/main/java/com/katbillings/pomodoro/controllers/Home.java
@@ -1,6 +1,7 @@
 package com.katbillings.pomodoro.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,32 +9,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Home {
 
     @GetMapping
-    public String hello() {
+    public String home(Model model) {
+        model.addAttribute("title","Home");
         return "index";
     }
 
     @GetMapping("create-account")
-    public String createAccount() {
+    public String createAccount(Model model) {
+        model.addAttribute("title","Create Account");
         return "create-account";
     }
 
     @GetMapping("login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("title","Login");
         return "login";
     }
 
     @GetMapping("edit-goal")
-    public String editGoal() {
+    public String editGoal(Model model) {
+        model.addAttribute("title","Edit Goal");
         return "edit-goal";
     }
 
     @GetMapping("timer")
-    public String timer() {
+    public String timer(Model model) {
+        model.addAttribute("title","Timer");
         return "timer";
     }
 
     @GetMapping("history")
-    public String history() {
+    public String history(Model model) {
+        model.addAttribute("title","History");
         return "history";
     }
 
